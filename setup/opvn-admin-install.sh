32f224a3fe1a1b1a13403c69edb0e4cfacdba45d
Leeeuijooo <euijoo3233@gmail.com>
2024-01-28 20:10:09 +0900

    Edit : new-ovpn-admin-bin-file
    Signed-off-by: Leeeuijooo <euijoo3233@gmail.com>

--- setup/opvn-admin-install.sh
+++ setup/opvn-admin-install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-file_name="ovpn-admin-linux-amd64.tar.gz"
+file_name="ovpn-admin.tar.gz"
 
 # 파일이 존재하지 않으면 다운로드 및 설치 수행
 if [ ! -f "$file_name" ]; then
     # ovpn-admin 다운로드 및 설치
-    wget https://github.com/flant/ovpn-admin/releases/download/2.0.2/$file_name
-    tar -xvzf $file_name
-    cp ovpn-admin-linux-amd64 /usr/sbin/
+    #wget https://github.com/flant/ovpn-admin/releases/download/2.0.2/$file_name
+    tar -xvzf ~/Openvpn_ovpn-admin/$file_name
+    cp ovpn-admin /usr/sbin/
     echo "Finished ovpn-admin file install"
 else
     echo "File $file_name already exists. Skipping download and install."
